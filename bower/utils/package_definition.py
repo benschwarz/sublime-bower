0e83e7ffcf67bd103c082fa871deee318637b835
Ben Schwarz <ben@germanforblack.com>
2013-04-04 23:23:57 +1100

    Handle packages that don't have a component.json more gracefully.

--- bower/utils/package_definition.py
+++ bower/utils/package_definition.py
@@ -24,8 +24,10 @@ class PackageDefinition:
             return os.path.join(self.projectdir, "components")
 
     def read(self, package_name):
-        print self.definition_path()
-        print package_name
         path = os.path.join(self.definition_path(), package_name, "component.json")
-        print path
-        return json.loads(open(path).read())
+        
+        if os.path.exists(path):
+            return json.loads(open(path).read())
+        else:
+            print "[Bower]: " + package_name + " doesn't have a component.json - Please submit an issue to the " + package_name + " project to tell the author." 
+            return {}
