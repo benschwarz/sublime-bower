0747f84fdffb6f4d71364710b5f1bce12c0b64d0
Ben Schwarz <ben@germanforblack.com>
2013-04-04 23:22:07 +1100

    Handle packages that don't have a 'main' attribute somewhat gracefully

--- bower_completions.py
+++ bower_completions.py
@@ -14,13 +14,25 @@ class BowerCompletions(sublime_plugin.EventListener):
         if not view.match_selector(locations[0], "text.html - source"):
             return []
 
-        print 'test'
-
         pt = locations[0] - len(prefix) - 1
         ch = view.substr(sublime.Region(pt, pt + 1))
+
         if ch != '`':
             return []
 
-        self.packageDefinition = PackageDefinition(os.path.join(view.window().folders()[0]))
+        packages = self.build_package_list(os.path.join(view.window().folders()[0]))
+            
+        return (packages, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
-        return ([self.get_completion(self.packageDefinition.read(pkg)["main"], pkg) for pkg in self.packageDefinition.get_installed_packages()], sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+    def build_package_list(self, path):
+        self.packageDefinition = PackageDefinition(path)
+
+        packages = []
+        for pkg in self.packageDefinition.get_installed_packages():
+            if self.packageDefinition.read(pkg).has_key('main'):
+                packages.append(self.get_completion(self.packageDefinition.read(pkg)["main"], pkg))
+            else:
+                print "[Bower]: " + pkg + " doesn't have a correctly formatted component.json file (missing 'main')"
+        return packages
+
+
