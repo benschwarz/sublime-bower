f1c6281069a4010842d29bf7fe3c9983cedd057b
Ben Schwarz <ben@germanforblack.com>
2013-04-06 17:17:12 +1100

    Import correct modules for Python 3

--- bower/utils/api.py
+++ bower/utils/api.py
@@ -1,17 +1,28 @@
-import urllib2
 import json
 import gzip
 from StringIO import StringIO
 import sublime
 
+try:
+  # ST3
+  import urllib.request as req
+except ImportError: 
+  import urllib2 as req
+
+try:
+  # ST3
+  from io import StringIO 
+except ImportError:
+  from StringIO import StringIO
+
 class API():
   def get(self, endpoint, *args):
     host = "http://bower.herokuapp.com/"
-    request = urllib2.Request(host + endpoint)
+    request = req.Request(host + endpoint)
     request.add_header('Accept-encoding', 'gzip')
 
     try:
-      response = urllib2.urlopen(request)
+      response = req.urlopen(request)
     except:
       sublime.error_message('Unable to connect to ' + host + endpoint + ". Check your internet connection.")
 
