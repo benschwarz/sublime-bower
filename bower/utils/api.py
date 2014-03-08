b0bd168abad2c1a7cfe7fb5379dd677002391b93
Ben Schwarz <ben@germanforblack.com>
2014-03-08 23:00:02 +1300

    Force decode of utf-8 and remove gzip handling

--- bower/utils/api.py
+++ bower/utils/api.py
@@ -21,21 +21,15 @@ class API():
     def get(self, endpoint, *args):
         host = "http://bower.herokuapp.com/"
         request = req.Request(host + endpoint)
-        request.add_header('Accept-encoding', 'gzip')
 
         try:
             response = req.urlopen(request)
         except urllib.error.HTTPError:
             sublime.error_message('Unable to connect to ' + host + endpoint + ". Check your internet connection.")
 
-        if response.info().get('Content-Encoding') == 'gzip':
-            buf = StringIO(response.read())
-            f = gzip.GzipFile(fileobj=buf)
-            responseText = f.read()
-        else:
-            responseText = response.read()
+        responseText = response.read().decode('utf-8', 'replace')
 
         try:
-            return json.loads(responseText.decode())
+            return json.loads(responseText)
         except:
             sublime.error_message('Oh Snap! It looks like theres an error with the Bower API.')
