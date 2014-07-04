efd4766c5d1191f58ff12b27020358a35cc5351a
Wes Bos <wesbos@gmail.com>
2014-07-04 18:10:57 -0400

    Update search URL
    Bower search is now housed at http://bower.io/search/

--- bower/commands/discover.py
+++ bower/commands/discover.py
@@ -3,4 +3,4 @@ import sublime_plugin
 
 class DiscoverPackageCommand(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.run_command('open_url', {'url': 'http://sindresorhus.com/bower-components/'})
+        self.window.run_command('open_url', {'url': 'http://bower.io/search/'})
