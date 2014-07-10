19ba93eb10e5b7f6d3426b41f45078a0198ef877
Ben Schwarz <ben@germanforblack.com>
2014-07-10 23:05:39 +1000

    Capture the bowerc path more gracefully. (ST2 fix)

--- bower/commands/bowerrc.py
+++ bower/commands/bowerrc.py
@@ -7,19 +7,20 @@ import os
 class BowerrcCommand(sublime_plugin.WindowCommand):
     def run(self):
         try:
-            project_file_path = self.window.project_file_name()
+            bowerrc = path()
 
-            if not project_file_path:
-                path = os.path.join(self.window.folders()[0], '.bowerrc')
-            else:
-                path = os.path.join(os.path.dirname(project_file_path), '.bowerrc')
-
-            if not os.path.exists(path):
+            if not os.path.exists(bowerrc):
                 rc = json.dumps({'directory': 'components'}, indent=2)
 
-                f = open(path, 'w+')
+                f = open(bowerrc, 'w+')
                 f.write(rc)
 
-            self.window.open_file(path)
+            self.window.open_file(bowerrc)
         except IndexError:
             sublime.error_message('Oh Dear! I need a directory for file .bowerrc.')
+    def path
+        try:
+            project_file_path = self.window.project_file_name()
+            return os.path.join(os.path.dirname(project_file_path), '.bowerrc')
+        except:
+            return os.path.join(self.window.folders()[0], '.bowerrc')
